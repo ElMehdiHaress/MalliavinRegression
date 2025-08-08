d0331f5631bf057d490758503d9ec1bfff840526
El Mehdi Haress <el-mehdi.haress@centralesupelec.fr>
2025-08-09 00:26:46 +0100

    feat: public API exports in __init__

--- src/malliavin_regression/__init__.py
+++ src/malliavin_regression/__init__.py
@@ -1,1 +1,30 @@
-"""Malliavin Regression core package"""
+"""
+Public API for malliavin_regression.
+"""
+
+from .core import (
+    brownian,
+    payoff1D,
+    generate_data,
+    f0,
+    h2_phi,
+    h3_phi,
+    gradient_free_descent,
+    gradient_free_f,
+    gradient_descent,
+    f,
+)
+
+__all__ = [
+    "brownian",
+    "payoff1D",
+    "generate_data",
+    "f0",
+    "h2_phi",
+    "h3_phi",
+    "gradient_free_descent",
+    "gradient_free_f",
+    "gradient_descent",
+    "f",
+]
+
